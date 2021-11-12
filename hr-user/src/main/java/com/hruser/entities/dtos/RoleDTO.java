f4bcc6ea862e67bea4c93f55cdad31e0de90cca4
Erick Batista <batista.erick@outlook.com>
2021-11-12 14:54:05 -0300

    Implementacao do hr-user

---
+++ hr-user/src/main/java/com/hruser/entities/dtos/RoleDTO.java
@@ -0,0 +1,57 @@
+package com.hruser.entities.dtos;
+
+public class RoleDTO {
+
+    private Long id;
+
+    private String roleName;
+
+    public RoleDTO() {
+    }
+
+    public RoleDTO(Long id, String roleName) {
+        this.id = id;
+        this.roleName = roleName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        RoleDTO other = (RoleDTO) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
