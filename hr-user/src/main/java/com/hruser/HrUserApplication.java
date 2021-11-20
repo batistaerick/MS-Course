bee0c178078e8a98d2d419a58f0a9eda251e82bc
Erick Batista <batista.erick@outlook.com>
2021-11-19 21:22:23 -0300

    Login, JWT Token

--- hr-user/src/main/java/com/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/hruser/HrUserApplication.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableEurekaClient
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
@@ -14,10 +11,5 @@ public class HrUserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
-	}
-
-	@Bean
-	public PasswordEncoder getPasswordEncoder() {
-		return new BCryptPasswordEncoder();
 	}
 }
